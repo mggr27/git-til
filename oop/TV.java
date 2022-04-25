77c6c5db158a8b5fa28ed6e98560cbc06c164d0e
mggr27 <84060918+mggr27@users.noreply.github.com>
2022-04-26 04:18:42 +0900

    Update TV.java

--- oop/TV.java
+++ oop/TV.java
@@ -1,24 +1,45 @@
-package oop;
+/**
+ * TV객체가 될 클래스입니다
+ * @author 오문정
+ * @version 1.0
+ *
+ */
 
 public class TV {
 	boolean power;
 	int channel;
 	int volume;
 	public boolean isPower() {		
-		return power; //현재사용중인 TV객체의 power값
+		return this.power; //현재사용중인 TV객체의 power값
 	}
-	public void setChannel(int ch) {
-		channel = ch;		
+	/**
+	 * 채널을 설정한다
+	 * @param channel 설정할 채널값
+	 */
+	public void setChannel(int channel) {
+		this.channel = channel;		
 	}
+	/**
+	 * 채널값을 반환한다
+	 * @return 채널값
+	 */
 	public int getChannel() {
 		return channel;		
 	}
+	/**
+	 * 음량을 1증가시킨다
+	 */
 	public void volumeUp() {
 		volume++;		
 	}
+	/**
+	 * 음량을 반환한다
+	 * @return 음량값
+	 */
 	public int getVolume() {
 		return volume;
 	}
 	
 
 }
+
