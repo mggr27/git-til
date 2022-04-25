59c757c08793a2d1bf56d223ba881ca87c8c27b1
mggr27 <84060918+mggr27@users.noreply.github.com>
2022-04-26 04:33:32 +0900

    Add files via upload

---
+++ CallByTest.java
@@ -0,0 +1,24 @@
+
+public class CallByTest {
+	public static int m(int i) {
+//	 System.out.println(i); //10
+		i =99;
+		return i;
+	}
+	public static void m(int[] arr) {
+		arr[0] = 99;
+	}
+	public static void main(String[] args) {
+		int i=10;
+		int j = m(i);
+		System.out.println(i); //10
+		System.out.println(j); //99
+		
+		int[] arr = {10,20,30};
+		m(arr); //인자의 자료형에 따라 메서드호출 
+		//byte b=7; m(b); m(int i)메서드호출 자동형변환
+		//float f=7.8F; m(f); 컴파일에러 double없고 자동형변환불가
+		System.out.println(arr[0]);
+	}
+
+}
