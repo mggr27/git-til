60a23c9d2d39f00af06978af4f38fa9b927f8127
mggr27 <84060918+mggr27@users.noreply.github.com>
2022-04-27 07:25:34 +0900

    Update Struct.java

--- basic/Struct.java
+++ basic/Struct.java
@@ -2,24 +2,23 @@
 public class Struct {
 
 	public static void main(String[] args) {
+		
 		//배열: 동일자료의 데이터묶음, 크기고정, 길이얻기length
 		int[] arr = new int[10];
 		arr[0] =5;
 		arr[1] =8;
-//		arr[2] ="HELLO";
-//		arr[10] = 999;
+//		arr[2] ="HELLO"; //동일 자료형만
+//		arr[10] = 999; //존재하지 않은 인덱스
 		
 		//맵: 키와 값을 저장, 크기자동변경, 길이얻기 size()
 		java.util.Map map;
 		map = new java.util.HashMap();
-		map.put("name", "오문정");
-		map.put("addr", "송파");
-		map.put("sal", 3000);
+		map.put("name", "몰리"); //크기1
+		map.put("addr", "송파"); //크기2
+		map.put("sal", 3000); //크기3
 		System.out.println(map.size()); //3
-		System.out.println(map.get("name"));//오문정
+		System.out.println(map.get("name")); //몰리
 		System.out.println(map.get("sal")); //3000
-		
-
+	
 	}
-
 }
