6d707e9642452de3f9c257df7b4df208c9fe04b6
mggr27 <84060918+mggr27@users.noreply.github.com>
2022-04-29 13:39:24 +0900

    Update CallByTest.java

--- oop/CallByTest.java
+++ oop/CallByTest.java
@@ -1,4 +1,3 @@
-
 public class CallByTest {
 	public static int m(int i) {
 //	 System.out.println(i); //10
@@ -14,11 +13,11 @@ public class CallByTest {
 		System.out.println(i); //10
 		System.out.println(j); //99
 		
-		int[] arr = {10,20,30};
+		int[] arr = {10,20,30}; //callbyvalue 값을 전달하는방식, 직접그 값 그대로 전달
 		m(arr); //인자의 자료형에 따라 메서드호출 
 		//byte b=7; m(b); m(int i)메서드호출 자동형변환
 		//float f=7.8F; m(f); 컴파일에러 double없고 자동형변환불가
 		System.out.println(arr[0]);
+		
 	}
-
 }
