430887e90fd620dd1ba9be0cc4c11c6c12ad2a89
mggr27 <funfun36@gmail.com>
2022-05-04 07:57:23 +0900

    [Add] world

---
+++ oop/world/CountryTest.java
@@ -0,0 +1,32 @@
+package world;
+//import world.asia.*;
+import world.asia.Korea;
+import world.asia.Japan;
+import java.util.Date;
+import java.util.Scanner;
+//import java.sql.Date;
+//import java.lang.String; //컴파일시 자동포함
+public class CountryTest {
+
+	public static void main(String[] args) {
+//		world.asia.Korea k;
+//		k = new world.asia.Korea();
+				
+		Korea k;
+		k = new Korea();
+		
+		Japan j = new Japan();
+		
+		Date d1 = new Date();
+		Scanner sc = new Scanner(System.in);
+		
+		java.sql.Date d2;
+		
+//		k.language = "일본어";
+//		k.capital = "평양";
+//		k.population = -1;
+		k.setPopulation(-1);
+
+	}
+
+}
