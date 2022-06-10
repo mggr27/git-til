16755c73ae18c277ff7e6c5a3df408a6eed9237f
mggr27 <84060918+mggr27@users.noreply.github.com>
2022-06-11 08:45:42 +0900

    Update WrapperTest.java

--- oop/default/WrapperTest.java
+++ oop/default/WrapperTest.java
@@ -18,15 +18,33 @@ import java.util.Date;
   참조형->기본형으로 바꿔주는 방법  
     int k = inte.intValue();
     
- * @author KOSTA
+ * @author 
  *
  */
 public class WrapperTest {
 	public static void main(String[] args) {
-		Object []arr = new Object[5];
+		int i = 34;
+//		System.out.println(i.toString()); //기본형은 기능이 없다
+//		System.out.println(i.a); //기본형은 상태값도 없다
+		Integer inte = new Integer(i);
+		inte.toString();
+		System.out.println(Integer.MIN_VALUE); //기본형int의 최소값
+		System.out.println(Integer.MAX_VALUE); //기본형int의 최대값
+		
+		Object[] arr = new Object[5];
 		arr[0] = new String("문자열");
 		arr[1] = new Date();
-		arr[2] = 1;
+//		arr[2] = new Integer(i); //기본형과 참조형은 형변환 안됨 ex) arr[2] = i; (x)
+		arr[2] = i; //AutoBoxing : 컴파일러에의해 arr[2] = new Integer(i);로 바뀜
+		
+//		arr[3] = new Character('A');
+		arr[3] = 'A'; //AutoBoxing :arr[3] = new Character('A');로 바뀜
+		
+//		int j = arr[2]; //참조형->기본형 형변환 안됨
+		int j = (Integer)arr[2]; //AutoUnBoxing : 컴파일러에의해 
+		//Integer inte2 = (Integer)arr[2];
+		//int j = inte2.intValue();로 바뀜
+		int k = (int)arr[2]; //권장안함
 
 	}
 
