99d153d018cf223f449cd2d7be3d7a2ca656831b
mggr27 <84060918+mggr27@users.noreply.github.com>
2022-07-09 09:22:39 +0900

    Update Product.java

--- oop/com.my.dto/Product.java
+++ oop/com.my.dto/Product.java
@@ -1,13 +1,17 @@
 package com.my.dto; //DataTransferObject 자료전달객체
 
 import java.util.Date;
+import java.util.Objects;
 
 public class Product {
+
 	private String prodNo;
 	private String prodName;
 	private int prodPrice;
 	private String prodInfo;
 	private Date prodMfd;
+	
+	public Product() {}
 
 	public Product(String prodNo, String prodName, int prodPrice) {
 		this(prodNo, prodName, prodPrice, null, null);
@@ -34,6 +38,69 @@ public class Product {
 
 	public void print() {
 		System.out.println(
-				"상품번호: " + prodNo + "상품명: " + prodName + "가격: " + prodPrice + "상세정보: " + prodInfo + "제조일자:" + prodMfd);
+		"상품번호: " + prodNo + "상품명: " + prodName + "가격: " + prodPrice + "상세정보: " + prodInfo + "제조일자:" + prodMfd);
 	}
+	
+	@Override
+	public String toString() {
+		return "상품번호: " + prodNo + ", 상품명: " + prodName + ", 가격: " + prodPrice;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(prodNo);
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		return Objects.equals(prodNo, other.prodNo); //return prodNo.equals(other.prodNo);
+	}
+	
+	public String getProdNo() {
+		return prodNo;
+	}
+	public void setProdNo(String prodNo) {
+		this.prodNo = prodNo;
+	}
+
+	public String getProdName() {
+		return prodName;
+	}
+
+	public void setProdName(String prodName) {
+		this.prodName = prodName;
+	}
+
+	public int getProdPrice() {
+		return prodPrice;
+	}
+
+	public void setProdPrice(int prodPrice) {
+		this.prodPrice = prodPrice;
+	}
+
+	public String getProdInfo() {
+		return prodInfo;
+	}
+
+	public void setProdInfo(String prodInfo) {
+		this.prodInfo = prodInfo;
+	}
+
+	public Date getProdMfd() {
+		return prodMfd;
+	}
+
+	public void setProdMfd(Date prodMfd) {
+		this.prodMfd = prodMfd;
+	}
+	
+	
 }
