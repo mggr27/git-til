9bf36e18dcb25c1ca46ad0921f2c161d114d13bb
mggr27 <84060918+mggr27@users.noreply.github.com>
2022-07-16 14:15:57 +0900

    Add files via upload

---
+++ oop/thread/ShareTest.java
@@ -0,0 +1,58 @@
+package thread;
+
+class Share{
+	int i;
+	Share(int i){
+		this.i = i;
+	}
+	public void push() {
+		synchronized(this) {
+			System.out.print("before push:i=" + i);
+			i++;
+			System.out.println(", after push:i=" + i);
+		}
+	}
+	public void pop() {
+		synchronized(this) {
+			System.out.print("before pop:i=" + i);
+			i--;
+			System.out.println(", after pop:i=" + i);
+		}
+	}
+}
+class Push extends Thread{
+	Share s;
+	Push(Share s){
+		this.s = s;
+	}
+	public void run() {
+		for(int i=0; i<10; i++) {
+			s.push();
+		}
+	}
+}
+class Pop extends Thread{
+	Share s;
+	Pop(Share s){
+		this.s = s;
+	}
+	public void run() {
+		for(int i=0; i<10; i++) {
+			s.pop();
+		}
+	}
+}
+public class ShareTest {
+	public static void main(String[] args) {
+		Share s = new Share(7);
+//		s.i = 7;
+		Push push = new Push(s);
+		Pop pop = new Pop(s);
+//		Pop pop = new Pop();
+//		pop.s = s;
+		
+		push.start();
+		pop.start();
+	}
+
+}
