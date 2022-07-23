12b10ec6869b2e3149502d6177afe4bf9b7ba078
mggr27 <84060918+mggr27@users.noreply.github.com>
2022-07-24 01:08:46 +0900

    Add files via upload

---
+++ oop/io/ObjectIOTest.java
@@ -0,0 +1,99 @@
+package io;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Scanner;
+
+
+class A implements Serializable{
+	int i;
+	A(int i){
+		this.i = i;
+	}
+}
+
+public class ObjectIOTest {
+	public static void write(String fileName) {
+		/*
+		 * dest : fileName파일
+		 * node stream : FileOutputStream
+		 * filter stream : ObjectOutputStream
+		 */
+		ObjectOutputStream oos = null;
+		try {
+			oos = new ObjectOutputStream(new FileOutputStream(fileName));
+			oos.writeObject(new Date());
+			A a = new A(7);
+			oos.writeObject(a);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if(oos !=null) {
+				try {
+					oos.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	public static void read(String fileName) {
+		/*
+		 * resource: fileName파일
+		 * node stream: FileInputStream
+		 * filter stream: ObjectInputStream
+		 */
+		ObjectInputStream ois = null;
+		try {
+			ois = new ObjectInputStream(new FileInputStream(fileName));
+			Object obj = ois.readObject();
+			System.out.println(obj); //obj.toString()자동호출됨
+			
+			Object obj1 = ois.readObject();
+			A a = (A)obj1;
+			System.out.println(a.i);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} finally {
+			if(ois !=null) {
+				try {
+					ois.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("객체쓰기-1, 객체읽기-2, 종료-9");
+		int opt = sc.nextInt();
+		String fileName = "a.ser";
+		switch(opt) {
+		case 1:
+			write(fileName);
+			break;
+		case 2:
+			read(fileName);
+			break;
+		}
+//		write(fileName);
+		//read(fileName);
+		
+	}
+
+}
