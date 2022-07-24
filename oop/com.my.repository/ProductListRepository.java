e3fa3e731dfa71121f511691054ce8861fa5716e
mggr27 <84060918+mggr27@users.noreply.github.com>
2022-07-25 00:30:32 +0900

    Update ProductListRepository.java

--- oop/com.my.repository/ProductListRepository.java
+++ oop/com.my.repository/ProductListRepository.java
@@ -47,13 +47,23 @@ public class ProductListRepository {
 //			throw new AddException("이미 존재하는 상품입니다");
 		
 	}
+	/**
+	 * 상품을 모두 검색한다
+	 * @return 상품들
+	 * @throws FindException 상품이 없으면 "상품이 없습니다"상세메시지를 갖는 예외가 발생한다
+	 */
 	public List<Product> selectAll() throws FindException{
 		if(products.size() ==0) {
 			throw new FindException("상품이 없습니다");
 		}
 		return products;
 	}
-	
+	/**
+	 * 상품번호로 상품검색한다
+	 * @param prodNo 상품번호
+	 * @return 상품객체
+	 * @throws FindException 상품번호에 해당하는 상품이 없으면 "상품이 없습니다"상세메시지를 갖는 예외가 발생한다
+	 */
 	public Product selectByProdNo(String prodNo) throws FindException{
 //		for(int i=0; i<products.size(); i++) {
 //			Product p = products.get(i);
@@ -64,7 +74,12 @@ public class ProductListRepository {
 		}
 		throw new FindException("상품이 없습니다");
 	}
-	
+	/**
+	 * 상품번호나 상품명에 검색어를 포함한 상품들을 반환한다
+	 * @param word 검색어
+	 * @return
+	 * @throws FindException 검색어를 포함한 상품들이 없으면 "검색어를 포함한 상품이 없습니다"라는 상세메시지를 갖는 예외가 발생한다 
+	 */
 	public List<Product> selectByProdNoOrProdName(String word) throws FindException {
 		List<Product> resultList = new ArrayList<>();
 		for(int i=0; i<products.size(); i++) {
