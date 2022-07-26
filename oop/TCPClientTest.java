b57f2113de30a5b998ad5ad386f1dc98b015174f
mggr27 <84060918+mggr27@users.noreply.github.com>
2022-07-26 23:17:19 +0900

    Add files via upload

---
+++ oop/TCPClientTest.java
@@ -0,0 +1,33 @@
+package net.client;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class TCPClientTest {
+	public static void main(String[] args) {
+		String serverIP = "127.0.0.1";
+		int serverPORT = 5432;
+		Socket s = null;
+		DataOutputStream dos = null;
+		try {
+			s = new Socket(serverIP, serverPORT);
+			dos = new DataOutputStream(s.getOutputStream());
+			String sendData = "Hello";
+			dos.writeUTF(sendData);
+			
+		} catch (UnknownHostException e) {
+//			e.printStackTrace();
+			System.out.println("IP가 잘못되었거나 호스트명이 잘못되었습니다");
+		} catch(ConnectException e) {
+			System.out.println("서버와의 연결이 실패되었습니다");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ oop/TCPServerTest.java
@@ -0,0 +1,32 @@
+package net.server;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.BindException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TCPServerTest {
+	public static void main(String[] args) {
+		int port = 5432;
+		ServerSocket ss = null;
+		Socket s = null;
+		DataInputStream dis = null;
+		try {
+			//1. port열기
+			ss = new ServerSocket(port);
+			
+			//2. 클라이언트기다리기, 소켓생성
+			s = ss.accept();
+			dis = new DataInputStream(s.getInputStream());
+			String receiveData = null;
+			receiveData = dis.readUTF();
+			System.out.println("클라이언트가 보내준 내용:" + receiveData);
+		} catch (BindException e) {
+			System.out.println(port+"포트가 이미 사용중입니다.");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
