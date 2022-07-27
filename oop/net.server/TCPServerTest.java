6da9c01c14c24a125e1b62457815bc407f483e69
mggr27 <84060918+mggr27@users.noreply.github.com>
2022-07-27 13:46:54 +0900

    Update TCPServerTest.java

--- oop/net.server/TCPServerTest.java
+++ oop/net.server/TCPServerTest.java
@@ -16,26 +16,30 @@ public class TCPServerTest {
 		try {
 			//1. port열기
 			ss = new ServerSocket(port);
-			
-			//2. 클라이언트기다리기, 소켓생성
-			s = ss.accept();
-			dis = new DataInputStream(s.getInputStream());
-			String receiveData = null;
-			while(!(receiveData = dis.readUTF()).equals("quit")) {
-				System.out.println("클라이언트가 보내준 내용:" + receiveData);
+			while(true) {
+				try {
+					//2. 클라이언트기다리기, 소켓생성
+					s = ss.accept();
+					dis = new DataInputStream(s.getInputStream());
+					String receiveData = null;
+					while(!(receiveData = dis.readUTF()).equals("quit")) {
+						System.out.println("클라이언트가 보내준 내용:" + receiveData);
+					}
+				} catch (SocketException e) {
+//					System.out.println("소켓이 끊겼습니다. 클라이언트장애인가 확인하세요");
+				} catch(IOException e) {
+//					e.printStackTrace();
+				} finally {
+					System.out.println("클라이언트와 연결을 종료합니다");
+				}
 			}
-			
 //			receiveData = dis.readUTF();
 //			System.out.println("클라이언트가 보내준 내용:" + receiveData);
 			
 		} catch (BindException e) {
 			System.out.println(port+"포트가 이미 사용중입니다.");
-		} catch (SocketException e) {
-//			System.out.println("소켓이 끊겼습니다. 클라이언트장애인가 확인하세요");
 		} catch (IOException e) {
 			e.printStackTrace();
-		} finally {
-			System.out.println("클라이언트와 연결을 종료합니다");
 		}
 	}
 
